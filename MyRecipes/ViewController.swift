639b80e2fe92527b093d4afe8ef15171fb8562a8
Alicia Ngomo <alicia_ngomo@hotmail.com>
2020-04-19 19:20:57 +0100

    Initial Commit

---
+++ MyRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRecipes
+//
+//  Created by Alicia Ngomo Fernandez on 19/04/2020.
+//  Copyright © 2020 Alicia Ngomo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
